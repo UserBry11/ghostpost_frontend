2bbd1b96f7a5e07fcef76c1f2e2c5a79ecf65a06
Bryan Fernandez <bryfern710@gmail.com>
2020-03-25 22:13:20 -0500

    added bootstrap styling

--- ghostpost_spa/src/App.js
+++ ghostpost_spa/src/App.js
@@ -6,19 +6,11 @@ const API_HOST = 'http://localhost:8000/api/';
 class App extends Component {
   state = {
     datas: [],
+    popularData: [],
     switchA: false,
     switchB: true,
     switchR: false,
-  }
-
-  boastOnlyButton = () => {
-    let result = this.state.switchB
-    this.setState({ switchB: !result })
-  }
-
-  roastsOnlyButton = () => {
-    let result = this.state.switchR;
-    this.setState({ switchR: !result })
+    switchP: true,
   }
 
   allItemsButton = () => {
@@ -46,6 +38,30 @@ class App extends Component {
     // })
   }
 
+  boastOnlyButton = () => {
+    let result = this.state.switchB
+    this.setState({ switchB: !result })
+  }
+
+  roastsOnlyButton = () => {
+    let result = this.state.switchR;
+    this.setState({ switchR: !result })
+  }
+
+  popularItemsButton = () => {
+    let result = this.state.switchP
+    this.setState({ switchP: !result })
+
+    fetch( API_HOST + 'boastroast/popular/')
+    .then(response => response.json())
+    .then(responseData => {
+      console.log('responseData', responseData)
+      this.setState({
+          popularData: responseData,
+      })
+    })
+    .catch(error => console.log("Error: ", error))
+  }
 
   // createBoastHandler = () => {
   //   let meth_head = {
@@ -81,154 +97,206 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+        <div className="container">
+          <header className="p-3 mb-2 bg-light shadow rounded text-center">
+            <h1>Hello World, I'm Ghostpost!</h1>
+            <h3>powered by React!</h3>
+            <div>Requests sent to: {API_HOST}</div>
+            <h3>Ordered by Post-Date</h3>
+          </header>
 
-        <h1>Hello World, I'm Ghostpost!</h1>
-        <h3>powered by React!</h3>
-        <div>Requests sent to: {API_HOST}</div>
-        <h3>Ordered by Post-Date:</h3>
+          <div className="p-3 mb-2 bg-light shadow rounded row shadow rounded">
+            <section className="all col-sm">
+                {this.state.switchA ?
+                  <div>
+                    <button onClick={this.allItemsButton}>
+                      all Posts!!
+                    </button>
+                    <h3>Click to Expand!</h3>
+                  </div>
+                  :
+                  <div>
+                    <button onClick={this.allItemsButton}>
+                      clear All items
+                    </button>
+                    <h3>All items</h3>
+                    {
+                      this.state.datas.map(value =>
+                          <ul key={value.id}>title: {value.title}
+                            <li>id: {value.id}</li>
+                            <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                            <li>Content: {value.content}</li>
+                            <li>Upvotes: {value.upvotes}</li>
+                            <li>Downvotes: {value.downvotes}</li>
+                            <li>Post-Date: {value.post_date}</li>
+                          </ul> 
+                        )
+                    }
+                  </div>
+                }
+            </section>
 
-        <section className="all">
-          <p>
-            <button onClick={this.allItemsButton
-            }>{!this.state.switchA ? "Expand All Posts" : "Collapse All Posts"}</button>
-          </p>
+            <section className="boasts col-sm text-center">
+                {this.state.switchB ?
+                    <div>
+                      <button onClick={this.boastOnlyButton}>
+                        for BOASTS :^D
+                      </button>
+                      <h3>:^D</h3>
+                    </div>
+                    :
+                    <div>
+                      <button onClick={this.boastOnlyButton}>
+                        clear BOASTS :^D
+                      </button>
+                      <h3>Boasts</h3>
+                      {
+                        this.state.datas.filter( condition => condition.boolean === true ).map(value =>
+                            <ul key={value.id}>title: {value.title}
+                              <li>id: {value.id}</li>
+                              <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                              <li>Content: {value.content}</li>
+                              <li>Upvotes: {value.upvotes}</li>
+                              <li>Downvotes: {value.downvotes}</li>
+                              <li>Post-Date: {value.post_date}</li>
+                            </ul>
+                          )
+                      }
+                    </div>
+                }
+            </section>
 
-            {this.state.switchA ?
-              this.state.datas.map( value => 
-                  <ul key={value.id}>title: {value.title}
-                    <li>id: {value.id}</li>
-                    <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
-                    <li>Content: {value.content}</li>
-                    <li>Upvotes: {value.upvotes}</li>
-                    <li>Downvotes: {value.downvotes}</li>
-                    <li>Post-Date: {value.post_date}</li>
-                  </ul>
-                )
-              // this.state.datas.map( data_obj => 
-
-              //   Object.keys(data_obj).map( function(key){
-
-              //     let value = data_obj[key]
-
-              //     return (
-              //         <ul key={value.id}>title: {value.title}
-              //           <li>id: {value.id}</li>
-              //           <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
-              //           <li>Content: {value.content}</li>
-              //           <li>Upvotes: {value.upvotes}</li>
-              //           <li>Downvotes: {value.downvotes}</li>
-              //           <li>Post-Date: {value.post_date}</li>
-              //         </ul>
-              //       )
-              //     }
-              //   )
-              // )
-              : 
-              <div>
-                Nothing here. Click Button!
-              </div>
-            }
-
-        </section>
-
-        <section className="boasts">
-
-            {this.state.switchB ?
-
-                <button onClick={this.boastOnlyButton}>
-                  for BOASTS :^D
-                </button>
-                :
-                <div>
-                  <button onClick={this.boastOnlyButton}>
-                    clear BOASTS :^D
-                  </button>
-                  <h3>Boasts</h3>
-                  {
-                    this.state.datas.filter( condition => condition.boolean === true ).map(value =>
-                              <ul key={value.id}>title: {value.title}
-                                <li>id: {value.id}</li>
-                                <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
-                                <li>Content: {value.content}</li>
-                                <li>Upvotes: {value.upvotes}</li>
-                                <li>Downvotes: {value.downvotes}</li>
-                                <li>Post-Date: {value.post_date}</li>
-                              </ul>
-                      )
-                    // this.state.datas.map(outer =>
-                    //     outer.filter(inner => inner.boolean === true ).map(value =>
-
-                    //           <ul key={value.id}>title: {value.title}
-                    //             <li>id: {value.id}</li>
-                    //             <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
-                    //             <li>Content: {value.content}</li>
-                    //             <li>Upvotes: {value.upvotes}</li>
-                    //             <li>Downvotes: {value.downvotes}</li>
-                    //             <li>Post-Date: {value.post_date}</li>
-                    //           </ul>
-                    //       )
-                    //   )
-                  }
-                </div>
-            }
-      </section>
-
-      <section className="roasts">
-          {this.state.switchR ?
-          <div>
-            <button onClick={this.roastsOnlyButton}>
-              clear ROASTS :^(
-            </button>
-            <h3>Roasts</h3>
-            {
-              this.state.datas.filter(condition => condition.boolean === false).map(value =>
-                  <ul key={value.id}>title: {value.title}
-                    <li>id: {value.id}</li>
-                    <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
-                    <li>Content: {value.content}</li>
-                    <li>Upvotes: {value.upvotes}</li>
-                    <li>Downvotes: {value.downvotes}</li>
-                    <li>Post-Date: {value.post_date}</li>
-                  </ul>              
-                )
-
-
-              // this.state.datas.map(outer =>
-              //   outer.filter(inner => inner.boolean === false).map(value =>
-              //     <ul key={value.id}>title: {value.title}
-              //       <li>id: {value.id}</li>
-              //       <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
-              //       <li>Content: {value.content}</li>
-              //       <li>Upvotes: {value.upvotes}</li>
-              //       <li>Downvotes: {value.downvotes}</li>
-              //       <li>Post-Date: {value.post_date}</li>
-              //     </ul>
-              //     )
-              //   )
-            }
-
+            <section className="roasts col-sm text-center">
+                {this.state.switchR ?
+                  <div>
+                    <button onClick={this.roastsOnlyButton}>
+                      clear ROASTS :^(
+                    </button>
+                    <h3>Roasts</h3>
+                    {
+                      this.state.datas.filter(condition => condition.boolean === false).map(value =>
+                          <ul key={value.id}>title: {value.title}
+                            <li>id: {value.id}</li>
+                            <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                            <li>Content: {value.content}</li>
+                            <li>Upvotes: {value.upvotes}</li>
+                            <li>Downvotes: {value.downvotes}</li>
+                            <li>Post-Date: {value.post_date}</li>
+                          </ul>              
+                        )
+                    }
+                  </div>
+                  :
+                  <div>
+                    <button onClick={this.roastsOnlyButton}>
+                      for ROASTS :^(
+                    </button>
+                    <h3>:^(</h3>
+                  </div>
+                }
+            </section>
+        
+            <Popular
+              popularItemsButton={this.popularItemsButton}
+              switchP={this.state.switchP}
+              popularData={this.state.popularData}
+            />
           </div>
-            :
-          <button onClick={this.roastsOnlyButton}>
-            for ROASTS :^(
-          </button>
-          }
-      </section>
-      <Roasts/>
-
+        </div>
       </React.Fragment>
     )
   }
 }
 
-class Roasts extends Component {
+class Popular extends Component {
   render() {
     return (
-      <section>
-        Footnotes
+      <section className="popular col-sm text-right">
+        {this.props.switchP ?
+          <div>
+            <button onClick={this.props.popularItemsButton}>for Popular items !!</button>
+            <h3>Populate!</h3>
+          </div>
+          : 
+          <div>
+            <button onClick={this.props.popularItemsButton}>clear Popular items</button>
+            <h3>Popular items</h3>
+            <div> (from highest-rated 2 lowest)</div>
+            {
+              this.props.popularData.map(value =>
+                <ul key={value.id}>title: {value.title}
+                  <li>id: {value.id}</li>
+                  <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                  <li>Content: {value.content}</li>
+                  <li>Upvotes: {value.upvotes}</li>
+                  <li>Downvotes: {value.downvotes}</li>
+                  <li>Post-Date: {value.post_date}</li>
+              </ul>              
+              )
+
+            }
+          </div>
+        }
       </section>
     )
   }
 }
 
 export default App;
+
+
+
+
+
+// this.state.datas.map( data_obj => 
+
+//   Object.keys(data_obj).map( function(key){
+
+//     let value = data_obj[key]
+
+//     return (
+//         <ul key={value.id}>title: {value.title}
+//           <li>id: {value.id}</li>
+//           <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+//           <li>Content: {value.content}</li>
+//           <li>Upvotes: {value.upvotes}</li>
+//           <li>Downvotes: {value.downvotes}</li>
+//           <li>Post-Date: {value.post_date}</li>
+//         </ul>
+//       )
+//     }
+//   )
+// )
+
+// this.state.datas.map(outer =>
+//     outer.filter(inner => inner.boolean === true ).map(value =>
+
+//           <ul key={value.id}>title: {value.title}
+//             <li>id: {value.id}</li>
+//             <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+//             <li>Content: {value.content}</li>
+//             <li>Upvotes: {value.upvotes}</li>
+//             <li>Downvotes: {value.downvotes}</li>
+//             <li>Post-Date: {value.post_date}</li>
+//           </ul>
+//       )
+//   )
+
+// this.state.datas.map(outer =>
+//   outer.filter(inner => inner.boolean === false).map(value =>
+//     <ul key={value.id}>title: {value.title}
+//       <li>id: {value.id}</li>
+//       <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+//       <li>Content: {value.content}</li>
+//       <li>Upvotes: {value.upvotes}</li>
+//       <li>Downvotes: {value.downvotes}</li>
+//       <li>Post-Date: {value.post_date}</li>
+//     </ul>
+//     )
+//   )
