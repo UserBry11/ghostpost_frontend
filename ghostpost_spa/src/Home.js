d2208a8ef813896670c40a3724923e9139bf8cd7
Bryan Fernandez <bryfern710@gmail.com>
2020-03-27 17:43:29 -0500

    mostly added auto-rendering

---
+++ ghostpost_spa/src/Home.js
@@ -0,0 +1,364 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './App.css';
+
+const API_HOST = 'http://localhost:8000/api/';
+
+class Home extends Component {
+  state = {
+    datas: [],
+    popularData: [],
+    switchA: false,
+    switchB: !true,
+    switchR: !false,
+    switchP: true,
+    id_value: '',
+    // now: new Date()
+  }
+
+  allItemsButton = () => {
+
+    let OnOff = this.state.switchA
+    this.setState({switchA: !OnOff})
+    // let meth_head = {
+    //   method: "GET",
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   }
+    // }
+
+    // fetch( API_HOST + 'boastroast/')
+    // .then(response => response.json())
+    // .then(responseData => {
+    //   console.log(responseData)
+    //   this.setState({
+    //     status: [responseData],
+    //     switch: !OnOff
+    //   })
+    // })
+    // .catch( error => {
+    //   console.log("Error: ", error)
+    // })
+  }
+
+  boastOnlyButton = () => {
+    let result = this.state.switchB
+    this.setState({ switchB: !result })
+  }
+
+  roastsOnlyButton = () => {
+    let result = this.state.switchR;
+    this.setState({ switchR: !result })
+  }
+
+  popularItemsButton = () => {
+  
+    fetch( API_HOST + 'boastroast/popular/')
+    .then(response => response.json())
+    .then(responseData => {
+      console.log('responseData', responseData)
+      this.setState({
+        popularData: responseData,
+      })
+    })
+    .catch(error => console.log("Error: ", error))
+    
+    let result = this.state.switchP
+    this.setState({ switchP: !result })
+  }
+
+  handleUpvote = id => () => {
+    fetch(API_HOST + 'boastroast/upvote/?id=' + String(id))
+    .then(response => response.json())
+    .then(responseData => {
+      console.log(responseData)
+      this.setState({ datas: responseData})
+    })
+    .catch(error => console.log(error))
+    this.setState({ id_value: id})
+  }
+
+  handleDownvote = id => () => {
+    fetch(API_HOST + 'boastroast/downvote/?id=' + String(id))
+    .then(response => response.json())
+    .then(responseData => {
+      console.log(responseData)
+      this.setState({ datas: responseData})
+      }  
+    )
+    .catch( error => console.log(error))
+    // this.setState({ error: !this.state.error})
+  }
+
+  componentDidMount(){ 
+    let meth_head = {
+      method: "GET",
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }
+
+    fetch( API_HOST + 'boastroast/', meth_head)
+    .then(response => response.json())
+    .then(responseData => {
+      console.log(responseData)
+      this.setState({
+        datas: responseData,
+      })
+    })
+    .catch( error => {
+      console.log("Error: ", error)
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+        <div className="container">
+          <header className="row p-3 mb-2 bg-light shadow rounded text-center">
+            <div className="col align-self-end">
+
+              <Link to="/post" className="post">
+                <button className="btn btn-info btn-lg shadow">
+                  Create Post
+                </button>
+              </Link>
+            </div>
+
+            <div className="col">
+              <h1>Hello World, I'm Ghostpost!</h1>
+              <h3>powered by React & Django!</h3>
+              <div><strong>Client-side: </strong>React.js</div>
+              <div><strong>Server-side: </strong>Django-Rest-Framework</div>
+              <div>Requests sent to: {API_HOST}</div>
+            </div>
+
+            <div className="col align-self-end">
+              <h4>Developer: Bryan Fernandez</h4>
+            </div>
+          </header>
+
+          <div className="row p-3 mb-2 bg-light shadow rounded shadow rounded">
+            
+            <section className="all col-sm text-center">
+                {this.state.switchA ?
+                  <div>
+                    <button onClick={this.allItemsButton}>
+                      all Posts!!
+                    </button>
+                    <h3>Click to Expand!</h3>
+                  </div>
+                  :
+                  <div>
+                    <button onClick={this.allItemsButton}>
+                      clear All items
+                    </button>
+                    <h3>All items</h3>
+                    {
+                      this.state.datas.map(value =>
+                      <p className="card">
+                          <div className="card-header bg-dark text-white"><strong>title: </strong>{value.title}</div>
+                          <ul key={value.id} className="list-group shadow">
+                            <li className="list-group-item list-group-item-secondary">id: {value.id}</li>
+                            <li className="list-group-item list-group-item-primary">{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                            <li className="list-group-item list-group-item-secondary">Content: {value.content}</li>
+                            <li className="list-group-item list-group-item-danger">
+                              <button onClick={this.handleUpvote(value.id)} className="btn btn-primary">Upvote</button>
+                              <button onClick={this.handleDownvote(value.id)} className="btn btn-danger">Downvote</button>
+                            </li>
+                            <li className="list-group-item list-group-item-primary">Upvotes: {value.upvotes}</li>
+                            <li className="list-group-item list-group-item-secondary">Downvotes: {value.downvotes}</li>
+                            <li className="list-group-item list-group-item-info">Post-Date: {value.post_date}</li>
+                          </ul> 
+                    </p>
+                        )
+                    }
+                  </div>
+                }
+            </section>
+
+            <section className="boasts col-sm text-center">
+                {this.state.switchB ?
+                    <div>
+                      <button onClick={this.boastOnlyButton}>
+                        for BOASTS :^D
+                      </button>
+                      <h3>:^D</h3>
+                    </div>
+                    :
+                    <div>
+                      <button onClick={this.boastOnlyButton}>
+                        clear BOASTS :^D
+                      </button>
+                      <h3>Boasts</h3>
+                      {
+                        this.state.datas.filter( condition => condition.boolean === true ).map(value =>
+                          <p className="card">
+                          <div className="card-header bg-dark text-white"><strong>title: </strong>{value.title}</div>
+                          <ul key={value.id} className="list-group shadow">
+                            <li className="list-group-item list-group-item-secondary">id: {value.id}</li>
+                            <li className="list-group-item list-group-item-primary">{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                            <li className="list-group-item list-group-item-secondary">Content: {value.content}</li>
+                            <li className="list-group-item list-group-item-danger">
+                              <button onClick={this.handleUpvote(value.id)} className="btn btn-primary">Upvote</button>
+                              <button onClick={this.handleDownvote(value.id)} className="btn btn-danger">Downvote</button>
+                            </li>
+                            <li className="list-group-item list-group-item-primary">Upvotes: {value.upvotes}</li>
+                            <li className="list-group-item list-group-item-secondary">Downvotes: {value.downvotes}</li>
+                            <li className="list-group-item list-group-item-success">Post-Date: {value.post_date}</li>
+                          </ul> 
+                    </p>
+                          )
+                      }
+                    </div>
+                }
+            </section>
+
+            <section className="roasts col-sm text-center">
+                {this.state.switchR ?
+                  <div>
+                    <button onClick={this.roastsOnlyButton}>
+                      clear ROASTS :^(
+                    </button>
+                    <h3>Roasts</h3>
+                    {
+                      this.state.datas.filter(condition => condition.boolean === false).map(value =>
+                        <p className="card">
+                        <div className="card-header bg-dark text-white"><strong>title: </strong>{value.title}</div>
+                        <ul key={value.id} className="list-group shadow">
+                          <li className="list-group-item list-group-item-secondary">id: {value.id}</li>
+                          <li className="list-group-item list-group-item-primary">{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                          <li className="list-group-item list-group-item-secondary">Content: {value.content}</li>
+                          <li className="list-group-item list-group-item-danger">
+                            <button onClick={this.handleUpvote(value.id)} className="btn btn-primary">Upvote</button>
+                            <button onClick={this.handleDownvote(value.id)} className="btn btn-danger">Downvote</button>
+                          </li>
+                          <li className="list-group-item list-group-item-primary">Upvotes: {value.upvotes}</li>
+                          <li className="list-group-item list-group-item-secondary">Downvotes: {value.downvotes}</li>
+                          <li className="list-group-item list-group-item-success">Post-Date: {value.post_date}</li>
+                        </ul> 
+                  </p>
+                        )
+                    }
+                  </div>
+                  :
+                  <div>
+                    <button onClick={this.roastsOnlyButton}>
+                      for ROASTS :^(
+                    </button>
+                    <h3>:^(</h3>
+                  </div>
+                }
+            </section>
+        
+            <Popular
+              popularItemsButton={this.popularItemsButton}
+              switchP={this.state.switchP}
+              popularData={this.state.popularData}
+              handleUpvote={this.handleUpvote}
+              handleDownvote={this.handleDownvote}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+class Popular extends Component {
+  render() {
+    return (
+      <section className="popular col-sm text-center">
+        {this.props.switchP ?
+          <div>
+            <button onClick={this.props.popularItemsButton}>for Popular items !!</button>
+            <h3>Populate!</h3>
+          </div>
+          : 
+          <div>
+            <button onClick={this.props.popularItemsButton}>clear Popular items</button>
+            <h3>Popular items</h3>
+            {
+              this.props.popularData.map(value =>
+                <p className="card">
+                <div className="card-header bg-dark text-white"><strong>title: </strong>{value.title}</div>
+                <ul key={value.id} className="list-group shadow">
+                  <li className="list-group-item list-group-item-secondary">id: {value.id}</li>
+                  <li className="list-group-item list-group-item-primary">{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+                  <li className="list-group-item list-group-item-secondary">Content: {value.content}</li>
+                  <li className="list-group-item list-group-item-danger">
+                    <button onClick={this.props.handleUpvote(value.id)} className="btn btn-primary">Upvote</button>
+                    <button onClick={this.props.handleDownvote(value.id)} className="btn btn-danger">Downvote</button>
+                  </li>
+                  <li className="list-group-item list-group-item-primary">Upvotes: {value.upvotes}</li>
+                  <li className="list-group-item list-group-item-secondary">Downvotes: {value.downvotes}</li>
+                  <li className="list-group-item list-group-item-success">Post-Date: {value.post_date}</li>
+                </ul> 
+          </p>
+              )
+            }
+          </div>
+        }
+      </section>
+    )
+  }
+}
+
+export default Home;
+
+
+
+
+
+// this.state.datas.map( data_obj => 
+
+//   Object.keys(data_obj).map( function(key){
+
+//     let value = data_obj[key]
+
+//     return (
+//         <ul key={value.id}>title: {value.title}
+//           <li>id: {value.id}</li>
+//           <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+//           <li>Content: {value.content}</li>
+//           <li>Upvotes: {value.upvotes}</li>
+//           <li>Downvotes: {value.downvotes}</li>
+//           <li>Post-Date: {value.post_date}</li>
+//         </ul>
+//       )
+//     }
+//   )
+// )
+
+// this.state.datas.map(outer =>
+//     outer.filter(inner => inner.boolean === true ).map(value =>
+
+//           <ul key={value.id}>title: {value.title}
+//             <li>id: {value.id}</li>
+//             <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+//             <li>Content: {value.content}</li>
+//             <li>Upvotes: {value.upvotes}</li>
+//             <li>Downvotes: {value.downvotes}</li>
+//             <li>Post-Date: {value.post_date}</li>
+//           </ul>
+//       )
+//   )
+
+// this.state.datas.map(outer =>
+//   outer.filter(inner => inner.boolean === false).map(value =>
+//     <ul key={value.id}>title: {value.title}
+//       <li>id: {value.id}</li>
+//       <li>{value.boolean ? 'Boast :^D' : 'Roast :^('}</li>
+//       <li>Content: {value.content}</li>
+//       <li>Upvotes: {value.upvotes}</li>
+//       <li>Downvotes: {value.downvotes}</li>
+//       <li>Post-Date: {value.post_date}</li>
+//     </ul>
+//     )
+//   )
---
+++ ghostpost_spa/src/Post.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import { Link} from "react-router-dom";
+
+const API_HOST = 'http://localhost:8000/api/';
+
+class Post extends Component {
+
+    state = {
+        title: '',
+        boolean: true,
+        content: '',
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleBooleanChange = () => {
+        this.setState({
+            boolean: !this.state.boolean
+        })
+    }
+
+    postPostButton = event => {
+        event.preventDefault();
+
+        let meth_head = {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(this.state)
+        }
+        fetch( API_HOST + 'boastroast/', meth_head)
+        .then(response => response.json())
+        .then(responseData => {
+            console.log(responseData)
+            }
+        )
+        .catch(error => { 
+            console.log("Error: ", error)
+            }
+        )
+    }
+
+    render() {
+      return (
+        <React.Fragment>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous"
+            />
+            <div className="card">
+                <div className="card-header shadow">
+
+                <h1 className="card-title">Make a Post!</h1>
+                <Link to="/">
+                    <button type="button" className="btn btn-outline-info btn-sm">
+                        Back to Home
+                    </button>
+                </Link>
+            </div>
+
+            <div className="card-body shadow">
+
+                <form onSubmit={this.postPostButton}>
+
+                    <div className="form-group row">
+                        <label htmlFor="id_title" className="col-sm-2 col-form-label">Title:</label>
+                        <div className="col-auto">
+                            <input
+                                type="text"
+                                className="form-control shadow"
+                                id="inputEmail3"
+                                name="title"
+                                maxLength="30"
+                                onChange={this.handleChange}
+                                required 
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Content:</label>
+                        <div className="col-auto">
+                            <textarea
+                                className="form-control shadow"
+                                id="inputPassword3 id_content"
+                                cols="40" 
+                                rows="10" 
+                                name="content" 
+                                onChange={this.handleChange}
+                                required 
+                            >
+                            </textarea>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <div className="col-sm-2">Boast or Roast:</div>
+                        <div className="col-sm-10">
+                            <div className="form-check">
+                                <input
+                                    className="form-check-input shadow" 
+                                    type="checkbox" 
+                                    id="gridCheck1 id_boolean"
+                                    name="boolean"
+                                    onChange={this.handleBooleanChange}
+                                />
+                                <label className="form-check-label" htmlFor="gridCheck1">
+                                Check for Boast, Uncheck for Roast
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <div className="col-sm-10">
+                        <button type="submit" className="shadow btn btn-primary btn-lg">Post It!</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </React.Fragment>
+      )
+    }
+}
+
+export default Post;
